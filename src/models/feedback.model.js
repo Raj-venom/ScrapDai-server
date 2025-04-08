cca59da9651f954e708127dd103cac0cddba0325
Raj-venom <rajsingh.rsr9824@gmail.com>
2025-04-09 00:56:03 +0545

    feat: update feedback model; add required validation for order and simplify rating fields

--- src/models/feedback.model.js
+++ src/models/feedback.model.js
@@ -5,27 +5,25 @@ const feedbackSchema = new mongoose.Schema({
     order: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Order",
+        required: true
     },
     userRating: {
         type: Number,
-        required: [true, 'User rating is required'],
         min: [1, 'User rating must be at least 1'],
         max: [5, 'User rating must not exceed 5'],
     },
     userReview: {
         type: String,
-        required: true,
         trim: true
     },
     collectorRating: {
         type: Number,
-        required: [true, 'User rating is required'],
         min: [1, 'User rating must be at least 1'],
         max: [5, 'User rating must not exceed 5'],
     },
     collectorReview: {
         type: String,
-        required: true
+        trim: true
     }
 
 }, { timestamps: true });
